5a5401265883f88a9f4df3dc299bfb0f73b229ce
ZERO <pales7@live.com>
2024-10-06 23:18:02 +0900

    create project
    Developer Tool: Visual Studio 2019
    Target Framework: .NET Framework 4.7
    
    Add Package: VideoLibrary, MediaToolkit, log4net

---
+++ Youtube Downloader/Youtube Downloader/Frm_Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Youtube_Downloader
+{
+    public partial class Frm_Main : Form
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Main_Load(object sender, EventArgs e)
+        {
+            logger.Debug("Youtube Downloader started...");
+        }
+    }
+}
